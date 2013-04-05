ade5c4d61810f27f9fd63a420723ce0009c6e5a6
Matjaz Drolc <mdrolc@gmail.com>
2013-04-05 22:50:05 +0200

    Updating docs

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ localdevhost
 ============
 
 Localhost dashboard written in PHP and Javascript
+
+Features
+--------
+ - Markdown display
+ - File browser
+
+![Localdevhost screenshot](https://github.com/drola/localdevhost/raw/master/img/screenshot.jpg "Localdevhost screenshot")
+
+Dependencies
+------------
+- PHP 5.4
+- Composer
+
+Usage
+-----
+1. Configure paths in `index.php`
+2. Go to backend and install dependencies using Composer:
+
+        cd backend/
+        php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"
+        php composer.phar install
+
+3. Open `index.php` via browser
--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
 $app = require 'backend/bootstrap.php';
+
+//Directory exposed by browser
 $app['document_root'] = __DIR__.'/../';
+
+//Home page
 $app['index'] = __DIR__.'/EXAMPLE.md';
+
+//Relative path to the assets (js/img/css)
 $app['assets_dir'] = "/localdevhost/";
+
+//Relative path to the homepage, usually same as $app['assets']
 $app['home'] = "/localdevhost/";
+
+//Logs that you want to display. Must have read aceess.
 $app['logs'] = array(
-    array('/var/log/apache2/error.log', 'apache/error.log', "Error Log")
+    //array('/var/log/apache2/error.log', 'apache/error.log', "Error Log")
     );
 $app->run();
